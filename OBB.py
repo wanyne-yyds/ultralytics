b2c86083bec748877faa72313745f3219f6c0569
Linux-Wayne <kang907801129@icloud.com>
2024-06-06 03:43:07 +0000

    同步配置文件

--- OBB.py
+++ OBB.py
@@ -10,8 +10,14 @@ if __name__ == '__main__':
 
     # Load a model
     if platform.system().lower() == 'linux':
+        #TODO 需要更换 Conv 激活函数 ->  nn.ReLU6()
         model = YOLO('/home/ultralytics/ultralytics/cfg/models/v8/yolov8s-obb-mobilenetv2.yaml').load(
-            '/home/ultralytics/runs/obb/MobilenetV2_OBB/weights/best.pt')
+            '/home/ultralytics/runs/obb/MobilenetV2_OBB_SPPF5/weights/last.pt'
+        )
+        
+        #TODO 需要更换 Conv 激活函数 ->  nn.SiLU()
+        # model = YOLO('/home/ultralytics/ultralytics/cfg/models/v8/yolov8n-obb.yaml').load(
+        #     '/home/ultralytics/runs/obb/MEOBB_02/weights/last.pt')
 
     elif platform.system().lower() == 'windows':
         #TODO 需要更换 Conv 激活函数 ->  nn.ReLU6()
@@ -26,16 +32,16 @@ if __name__ == '__main__':
         raise NotImplementedError
     
     # Train the model
-    results = model.train(data='/home/ultralytics/ultralytics/cfg/datasets/MED.yaml', 
-                          epochs=160, 
-                          imgsz=480,
-                          batch=258,
-                          workers=32,
-                          name='MobilenetV2_OBB',
-                          device=[3, 2, 1])
+    # results = model.train(data='/home/ultralytics/ultralytics/cfg/datasets/MED.yaml', 
+    #                       epochs=160, 
+    #                       imgsz=480,
+    #                       batch=258,
+    #                       workers=32,
+    #                       name='MobilenetV2_OBB_SPPF5',
+    #                       device=[3, 2, 1])
 
     # Predict on an image
-    # results = model(r'Z:\dataset\Motorcycle_Stop_Line_Dataset\Src\Labels\Train\Forgery\BuleWhite_Train_forgery', 
+    # results = model(r'Z:\dataset\Motorcycle_Stop_Line_Dataset\Src\Labels\Train\BuleWhite_Train', 
     #                 mode='predict',
     #                 save=True,
     #                 show_labels=True, 
@@ -44,7 +50,10 @@ if __name__ == '__main__':
     #                 )
 
     # Validate the model
-    # metrics = model.val(data='/home/ultralytics/ultralytics/cfg/datasets/MED.yaml')  # no arguments needed, dataset and settings remembered
+    metrics = model.val(data='/home/ultralytics/ultralytics/cfg/datasets/MED.yaml', 
+                        imgsz=480,
+                        conf=0.5,
+                        )  # no arguments needed, dataset and settings remembered
 
     # Export onnx
     # model.export(format='onnx', opset=11, simplify=True, dynamic=False, imgsz=480)
